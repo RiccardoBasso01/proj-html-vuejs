5a90aa4006a82d04def0d4466e6a3bc66d3603ee
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-07-11 18:18:52 +0200

    Modifica logica cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,7 +6,41 @@ import TheJumbotron from './TheJumbotron.vue';
 export default {
     data() {
         return {
-
+            processCards: [
+                {
+                    title: 'First theres an idea',
+                    text: 'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself.'
+                },
+                {
+                    title: 'Then we talk about',
+                    text: 'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself.'
+                },
+                {
+                    title: 'And we think about',
+                    text: 'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself.'
+                },
+                {
+                    title: 'So we draw along',
+                    text: 'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself.'
+                }
+            ],
+            blogCards: [
+                {
+                    img: './src/assets/img/h-2-blog-img-1.jpg',
+                    title: 'This way, loves: a detailed guide through new design',
+                    text: 'By Emily Fields'
+                },
+                {
+                    img: './src/assets/img/h-2-blog-img-2.jpg',
+                    title: 'I try to give people a different way of looking art',
+                    text: 'By Emily Fields'
+                },
+                {
+                    img: './src/assets/img/h-2-blog-img-3.jpg',
+                    title: 'Introduce Richard Laperrière of those amazing features',
+                    text: 'By Emily Fields'
+                },
+            ]
         }
     },
     components: { TheSlider, BaseCard, TheJumbotron },
@@ -24,56 +58,30 @@ export default {
         <!-- slider -->
         <TheSlider />
 
-        <section id="big-card">
-            <div class="wrapper">
-                <BaseCard v-for="n in 6" :key="n">
-                    <img :src="getImagePath(`h-2-port-img-${n}.jpg`)" :alt="`portfolio ${n}`">
-                </BaseCard>
-            </div>
+        <!-- Gallery -->
+        <section class="wrapper">
+            <BaseCard v-for="n in 6" :key="n">
+                <img :src="getImagePath(`h-2-port-img-${n}.jpg`)" :alt="`portfolio ${n}`">
+            </BaseCard>
         </section>
 
-        <section id="working-process">
-            <div class="container-m">
-                <h2>Working <span class="txt-playfair-display">process</span></h2>
-                <div class="wrapper">
-
-                    <BaseCard v-for="n in 4" :key="n" :className="'card small'">
-                        <div class="card-flex">
-                            <div class="card-img-round">
-                                <img :src="getImagePath()" alt="">
-                            </div>
-                            <div class="description">
-                                <div class="card-title">
-                                    <h5>Titolo card</h5>
-                                </div>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, doloremque tempora omnis
-                                veritatis,
-                            </div>
-                        </div>
-                    </BaseCard>
-                </div>
-
+        <!-- Process Cards -->
+        <section class="container-m">
+            <h2>Working <span class="txt-playfair-display">process</span></h2>
+            <div class="wrapper">
+                <BaseCard :section="'process-cards'" v-for="(card, i) in processCards" :key="`Process card ${i}`"
+                    :className="'process card small'" :title="card.title" :text="card.text" />
             </div>
         </section>
 
         <!-- jumbotron -->
         <TheJumbotron />
 
-        <section id="blog-list">
-            <div class="container-m">
-                <div class="wrapper">
-
-                    <BaseCard v-for="n in 3" :key="n">
-                        <div class="img">
-                            <img :src="getImagePath(`h-2-blog-img-${n}.jpg`)" :alt="`blog ${n}`">
-                        </div>
-                        <div class="description">
-                            <h3>Card title ...</h3>
-                            <span>By emily</span>
-                        </div>
-                    </BaseCard>
-                </div>
-
+        <!-- Blog List -->
+        <section class="container-m">
+            <div class="wrapper">
+                <BaseCard :section="'blog-list'" v-for="(card, i) in blogCards" :key="`Blog card ${i}`" :blogImg="card.img"
+                    :title="card.title" :text="card.text" />
             </div>
         </section>
 
@@ -83,23 +91,4 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/style.scss' as *;
 @use '../assets/scss/_colors.scss' as *;
-
-.description {
-    font-size: var(--font-size-s);
-}
-
-.card-flex {
-    @include flex(column, center, center)
-}
-
-.card-img-round {
-    @include square(162px);
-    @include flex(row, center, center);
-    background-color: $bg-milk-glass;
-    border-radius: 50%;
-}
-
-#blog-list .card {
-    padding: 20px;
-}
 </style>
--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -2,13 +2,41 @@
 export default {
     props: {
         className: String,
+        section: String,
+        title: String,
+        text: String,
+        blogImg: String
     }
 }
 </script>
 
 <template>
     <div :class="className || 'card'">
+        <!-- Gallery -->
         <slot></slot>
+
+        <!-- Process Cards -->
+        <div v-if="section === 'process-cards'">
+            <div class="card-img">
+            </div>
+            <div class="description">
+                <div class="card-title">
+                    <h5>{{ title }}</h5>
+                </div>
+                <p>{{ text }}</p>
+            </div>
+        </div>
+
+        <!-- Blog List -->
+        <div v-if="section === 'blog-list'" class="p-2">
+            <div class="img">
+                <img :src="blogImg">
+            </div>
+            <div class="description">
+                <h3>{{ title }}</h3>
+                <span>{{ text }}</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,16 +44,24 @@ export default {
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/_colors.scss' as *;
 @use '../assets/scss/style.scss' as *;
 
-.card {
-    width: 100%;
-    border-radius: 0;
+.description {
+    font-size: var(--font-size-s);
+}
+
+.process.card>div {
+    @include flex(column, center, center)
+}
+
+.process .card-img {
+    @include rounded(162px);
+    background-color: $bg-milk-glass;
 }
 
 
-
-// Card Responsive
+//---------------------------------------------- Logica card responsive
 @media screen and (min-width: 700px) {
     .card {
         width: calc(100% / 2);
