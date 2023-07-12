c5a14a0d23a6ead34a571c7aad97d43cbae1d042
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-07-12 21:29:10 +0200

    Aggiunta effetti footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -16,22 +16,22 @@ export default {
             <div class="wrapper">
                 <BaseCard :className="'card small'">
                     <a href="#"><img src="../assets/img/logo-sidearea-1.png" alt="footer logo"></a>
-                    <div class="mt-1">Lets get crative</div>
+                    <div>Lets get crative</div>
                 </BaseCard>
 
                 <BaseCard :className="'card small'">
                     <div><a href="#">maree@qodeinteractive.com</a></div>
-                    <div><a href="#" class="mt-1">+44645 321 789</a></div>
+                    <div><a href="#">+44645 321 789</a></div>
                 </BaseCard>
 
                 <BaseCard :className="'card small'">
                     <div><a href="#">Avenue d’Auderghem 10</a></div>
-                    <div><a href="#" class="mt-1">1040 Brussels, Belgium</a></div>
+                    <div><a href="#">1040 Brussels, Belgium</a></div>
                 </BaseCard>
 
                 <BaseCard :className="'card small'">
                     <div>Stay in touch with us</div>
-                    <ul class="flex gap-3 mt-1">
+                    <ul class="flex gap-3">
                         <li><a href="#"><font-awesome-icon :icon="['fab', 'twitter']" /></a></li>
                         <li><a href="#"><font-awesome-icon :icon="['fab', 'pinterest-p']" /></a></li>
                         <li><a href="#"><font-awesome-icon :icon="['fab', 'facebook-f']" /></a></li>
@@ -45,6 +45,7 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/scss/_colors.scss' as *;
+@use '../assets/scss/style.scss' as *;
 
 footer {
     background-color: $bg-otto-ice;
@@ -52,7 +53,8 @@ footer {
 }
 
 .card {
-    text-align: start;
+    @include flex(space-between, flex-start, column);
+    gap: 10px;
     padding: 30px 0;
 }
 
@@ -61,6 +63,10 @@ img {
 }
 
 li {
-    color: $black;
+    color: $black; // icone
+}
+
+a {
+    @include text_underscore($bg-adorable, 7px, 0.2s);
 }
 </style>
